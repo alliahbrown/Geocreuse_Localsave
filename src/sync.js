5586d3b499808eb7c828d0d8874551fa49cbe8de
alliahbrown <formation@ubuntu.ubuntu-domain>
2026-04-28 14:38:59 +0200

    hotfix : release ci

--- src/sync.js
+++ src/sync.js
@@ -2,20 +2,33 @@ const { net } = require('electron');
 const db = require('./db');
 
 const API_URL = 'https://sauvegarde.leptitbraquet.fr';
-const API_KEY = process.env.API_SECRET_KEY
+// const API_KEY = process.env.API_SECRET_KEY
 
-console.log(API_KEY)
-function isOnline() {
-    return net.isOnline();
+
+function getApiKey() {
+    return process.env.API_SECRET_KEY;
 }
 
 async function fetchJson(path) {
     const res = await fetch(`${API_URL}${path}`, {
-        headers: { 'x-api-key': API_KEY }
+        headers: { 'x-api-key': getApiKey() }  // ← ici
     });
     if (!res.ok) throw new Error(`Erreur API ${path} : ${res.status}`);
     return res.json();
 }
+
+console.log(getApiKey())
+function isOnline() {
+    return net.isOnline();
+}
+
+// async function fetchJson(path) {
+//     const res = await fetch(`${API_URL}${path}`, {
+//         headers: { 'x-api-key': API_KEY }
+//     });
+//     if (!res.ok) throw new Error(`Erreur API ${path} : ${res.status}`);
+//     return res.json();
+// }
 
 async function pull() {
     if (!isOnline()) throw new Error('Hors ligne');
